4dba15cc5c9ec5af84a613ccb1890dcc7c3f16bd
Nzingani <isabelnzingani@outlook.com>
2018-07-27 09:33:53 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>P3ex2</title>
+  </head>
+  <body>
+    <?php
+    $variable = 0;
+   $variable1 = 20;
+while($variable < 80)
+{
+echo '$variable * $variable1';
+$variable ++;// incrémenter
+}
+  ?>
+</body>
+</html>
